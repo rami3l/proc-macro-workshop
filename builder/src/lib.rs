00995b6c31657e927d60b06c688da4133e0095b6
Rami3L <rami3l@outlook.com>
2020-06-25 00:21:52 +0800

    feat: update to checkpoint #2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,82 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{
+    parse_macro_input, parse_quote, Data, DeriveInput, Fields, GenericParam, Generics, Index,
+};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    // Parse the input tokens into a syntax tree.
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    // Used in the quasi-quotation below as `#name`.
+    let name = input.ident;
+    let vis = input.vis;
+
+    let builder_ty = Ident::new(&format!("{}Builder", name), Span::call_site());
+
+    let builder_fields = gen_builder_fields(&input.data);
+
+    let builder_fn_body = gen_builder_fn_body(&input.data);
+
+    let expanded = quote! {
+        #vis struct #builder_ty {
+            #builder_fields
+        }
+
+        impl #name {
+            #vis fn builder() -> #builder_ty {
+                #builder_ty {
+                    #builder_fn_body
+                }
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compiler.
+    proc_macro::TokenStream::from(expanded)
+}
+
+fn gen_builder_fields(data: &Data) -> TokenStream {
+    match *data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f.ident;
+                    let ty = &f.ty;
+                    quote_spanned! {f.span()=>
+                        #name: Option<#ty>,
+                    }
+                });
+                quote! {
+                    #(#recurse)*
+                }
+            }
+            Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    }
+}
+
+fn gen_builder_fn_body(data: &Data) -> TokenStream {
+    match *data {
+        Data::Struct(ref data) => match data.fields {
+            Fields::Named(ref fields) => {
+                let recurse = fields.named.iter().map(|f| {
+                    let name = &f.ident;
+                    quote_spanned! {f.span()=>
+                        #name: None,
+                    }
+                });
+                quote! {
+                    #(#recurse)*
+                }
+            }
+            Fields::Unnamed(_) | Fields::Unit => unimplemented!(),
+        },
+        Data::Enum(_) | Data::Union(_) => unimplemented!(),
+    }
 }
